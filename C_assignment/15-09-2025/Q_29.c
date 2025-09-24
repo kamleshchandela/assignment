d94a565bc2f5fcf885286c7ae7dc2596cb0e4614
Kamlesh <kamleshchandela51@gmail.com>
2025-09-24 20:47:15 +0530

    kids word and super over added

--- C_assignment/15-09-2025/Q_29.c
+++ C_assignment/15-09-2025/Q_29.c
@@ -13,6 +13,10 @@
 // Description: Full pyramid (Q13) + inverted pyramid (Q26). Hint: Print upper pyramid first, then lower. Difficulty: Medium
 
 
+
+// method 1
+
+
 #include <stdio.h>
 
 int main()
@@ -45,3 +49,44 @@ int main()
 
     return 0;
 }
+
+
+// method 2
+
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=(2*n)-1;j++){
+            if(j>=n-i+1 && j<=n+i-1){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    for(int i=1;i<=n-1;i++){
+        for(int j=1;j<=(2*n)-1;j++){
+            if(j>i && j<2*n-i){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
+
+
+
